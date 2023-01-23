cf3e98f1c9cf5537bcff573d2ce982c09f7f7e16
MQian21 <michaelymq@gmail.com>
2023-01-23 11:39:35 -0500

    connected to backend for add student button

--- src/frontend/src/client.js
+++ src/frontend/src/client.js
@@ -12,3 +12,11 @@ const checkStatus = response => {
 export const getAllStudents = () =>
     fetch("api/v1/students")
         .then(checkStatus);
+
+export const addNewStudent = student =>
+    fetch("api/v1/students", {
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        method: 'Post',
+        body: JSON.stringify(student)});
