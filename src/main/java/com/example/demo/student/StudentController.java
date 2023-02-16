95c2b441e39aba78ecc352f74ad1f2b658bfee92
MQian21 <michaelymq@gmail.com>
2023-02-16 02:55:21 -0500

    error handling

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,23 +1,29 @@
 package com.example.demo.student;
 
 import lombok.AllArgsConstructor;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping(path = "api/v1/students")
 @AllArgsConstructor
 public class StudentController {
-
     private final StudentService studentService;
     @GetMapping
     public List<Student> getAllStudents() {
         return studentService.getAllStudents();
     }
     @PostMapping
-    public void addStudent(@RequestBody Student student) {
+    public void addStudent(@RequestBody Student student){
         studentService.addStudent(student);
+    }
+    @DeleteMapping
+    @CrossOrigin(origins = "http://localhost:3000")
+    public void deleteStudent(@PathVariable("studentId") Long studentId) {
+        studentService.deleteStudent(studentId);
     }
 }
